f45c7df554314e27a29147975357c65af1ab4ac7
Guada8a <jochoa10@ucol.mx>
2024-12-08 22:15:00 -0600

    feat: agregar página 404 personalizada y eliminar notificaciones de Discord

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  site: 'https://guada8a.github.io/portfolio', // Reemplaza con la URL de tu GitHub Pages
-  base: '/portfolio/', // Reemplaza con el nombre de tu repositorio
-  outDir: 'dist', // Directorio de salida (puede ser cambiado según tu preferencia)
+  site: 'https://guada8a.github.io/portfolio',
+  base: '/portfolio/',
+  outDir: 'dist',
 });
